5a2a21bd1b6297a0a819cebe402a0197bf537400
Alan Wu <alan.c.wu@gmail.com>
2018-11-03 17:13:36 -0700

    15, 16

--- firestore/google/cloud/firestore_v1beta1/_helpers.py
+++ firestore/google/cloud/firestore_v1beta1/_helpers.py
@@ -1104,15 +1104,16 @@ def pbs_for_set(document_path, document_data, merge_paths, exists):
 #        merge_paths = list(merge_paths)
  #  if self._merge_paths:
         new_path = None
+        temp_paths = list(field_paths)
         for field_path in real_merge_paths:
             ancestor = field_path.common(real_merge_paths[0])
             if new_path and ancestor.parts > new_path.parts:
                 new_path = ancestor
         if not new_path:
             new_path = real_merge_paths[0]
-        real_merge_paths = [new_path]
-        real_merge_paths = list(set([field_path.to_api_repr() for field_path in real_merge_paths]))
-        mask = common_pb2.DocumentMask(field_paths=sorted(real_merge_paths))
+        temp_paths = [new_path]
+        temp_paths = list(set([field_path.to_api_repr() for field_path in temp_paths]))
+        mask = common_pb2.DocumentMask(field_paths=sorted(temp_paths))
         update_pb.update_mask.CopyFrom(mask)
         
 #        option.modify_write(update_pb, field_paths=merge_paths)
